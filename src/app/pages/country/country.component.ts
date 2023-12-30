d087429b8f815b34cac88ba5ed4c65ae1e1968de
Caio Henrique <caiohenriquessiqueira.caike@gmail.com>
2023-12-29 23:05:51 -0300

    Creating Responsive Details Page

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+// country.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from 'src/app/services/countries.service';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss']
 })
-export class CountryComponent {
+export class CountryComponent implements OnInit {
+  countryDetails: any;
 
+  constructor(private route: ActivatedRoute, private service: CountriesService) {}
+
+  ngOnInit(): void {
+    const countryName = this.route.snapshot.paramMap.get('name');
+
+    this.service.getCountryDetailsByName(countryName).subscribe(
+      (res) => {
+        this.countryDetails = res;
+      }
+    );
+  }
+
+  getCurrenciesString() {
+    if (this.countryDetails && this.countryDetails.currencies) {
+      const currencyNames = Object.keys(this.countryDetails.currencies).map(key => this.countryDetails.currencies[key].name);
+      return currencyNames.join(', ');
+    }
+    return '';
+  }
+
+  getLanguagesString() {
+    if (this.countryDetails && this.countryDetails.languages) {
+      const languageNames = Object.keys(this.countryDetails.languages).map(key => this.countryDetails.languages[key]);
+      return languageNames.join(', ');
+    }
+    return '';
+  }
 }
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,15 +1,28 @@
+// countries.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
-  private url: string = "https://restcountries.com/v3.1/";
-  
-  constructor(private http: HttpClient) { }
+  private url: string = "https://restcountries.com/v3.1";
 
-  getCountries() {
+  constructor(private http: HttpClient) {}
+
+  getCountries(): Observable<any> {
     return this.http.get(`${this.url}/all`);
+  }
+
+  getCountryDetailsByName(name: string | null): Observable<any> {
+    return this.getCountries().pipe(
+      map((countries: any[]) => countries.find((country: any) => country.name.common === name))
+    );
+  }
+
+  getCountryByCode(code: string): Observable<any> {
+    return this.http.get<any>(`${this.url}/alpha/${code}`);
   }
 }
